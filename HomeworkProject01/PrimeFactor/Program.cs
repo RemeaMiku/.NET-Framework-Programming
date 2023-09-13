7c3b24aa42c7cefba644387f6a32c240d96a4899
RemeaMiku <2020302142257@whu.edu.cn>
2023-09-13 21:58:12 +0800

    PrimeFactor改为外部输入

--- HomeworkProject01/PrimeFactor/Program.cs
+++ HomeworkProject01/PrimeFactor/Program.cs
@@ -1,3 +1,14 @@
 ﻿using PrimeFactor;
-foreach (var item in Solution.FindPrimeFactors(10000000000 + 114514))
-    Console.WriteLine(item);
+using static System.Console;
+while (true)
+{
+    WriteLine("请输入一个整数：");
+    var line = ReadLine();
+    if (!int.TryParse(line, out var input))
+        WriteLine("输入不合法，请重试");
+    else
+    {
+        WriteLine("素数因子有：");
+        WriteLine(string.Join(',', Solution.FindPrimeFactors(input)));
+    }
+}
